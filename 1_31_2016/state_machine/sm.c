666c17bdb83cd34a87f3f702cb46581958c82a03
BharathTalloju <tallojubharath@gmail.com>
2016-02-21 18:41:08 +0530

    Adios

--- 1_31_2016/state_machine/sm.c
+++ 1_31_2016/state_machine/sm.c
@@ -14,6 +14,36 @@ int str_equal(str1, str2){
     
 }
 
+int start_state(){
+    int flag = 1;
+    char command[500];
+    
+    while(flag){
+        scanf("%[^\n]s", command);
+        if(!parse_command(command)){
+            printf("\nInvalid Syntax");
+        }
+    }
+}
 
-
+int parse_command(char* command){
+    int result = 0;
+    int choice;
+    char word[20];
+    int command_reader = 0;
+    int word_writer = 0;
+    
+    while(command[command_reader] != ' '){
+        word[word_writer] = command[command_reader];
+        word_writer++;
+        command_reader++;
+    }word[word_writer] = '\0';
+    
+    if(str_equal(word, "merge")){
+        printf("\nMerge selected");
+        
+    }
+    
+    
+}
 
